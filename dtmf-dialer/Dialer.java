803b49fe4480a3b4d55b955d7e7678ac5a6d3faa
Nis Sarup <nis@sarup.dk>
2010-09-19 20:37:58 +0200

    Started a new class to do it all. Got a bit stuck on the fact that you can't do switch with strings. What on earth were they thinking?!

---
+++ dtmf-dialer/Dialer.java
@@ -0,0 +1,217 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Dialer {
+	String windowTitle = "Test";
+	JFrame mFrame;
+	
+	JPanel dPanel;
+	
+	Button btn0 = new Button();
+	Button btn1 = new Button();
+	Button btn2 = new Button();
+	Button btn3 = new Button();
+	Button btn4 = new Button();
+	Button btn5 = new Button();
+	Button btn6 = new Button();
+	Button btn7 = new Button();
+	Button btn8 = new Button();
+	Button btn9 = new Button();
+	Button btnStar = new Button();
+	Button btnHash = new Button();
+	Button btnA = new Button();
+	Button btnB = new Button();
+	Button btnC = new Button();
+	Button btnD = new Button();
+	
+	JLabel statusLabel = new JLabel();
+	
+	public static void main (String[] args) {
+		Dialer d = new Dialer();
+		d.init();
+	}
+	
+	public void init() {
+		mFrame = makeFrame();
+		dPanel = makePanel();
+		
+		mFrame.add(dPanel);
+		
+		addListeners();
+		
+		mFrame.setVisible(true);
+	}
+	
+	public void addListeners() {
+		class ButtonListener implements ActionListener {
+			public void actionPerformed (ActionEvent event) {
+				String st = "";
+				if (event.getSource() == btn0) { st = "Knap 0"; }
+				if (event.getSource() == btn1) { st = "Knap 1"; }
+				if (event.getSource() == btn2) { st = "Knap 2"; }
+				if (event.getSource() == btn3) { st = "Knap 3"; }
+				if (event.getSource() == btn4) { st = "Knap 4"; }
+				if (event.getSource() == btn5) { st = "Knap 5"; }
+				if (event.getSource() == btn6) { st = "Knap 6"; }
+				if (event.getSource() == btn7) { st = "Knap 7"; }
+				if (event.getSource() == btn8) { st = "Knap 8"; }
+				if (event.getSource() == btn9) { st = "Knap 9"; }
+				if (event.getSource() == btnA) { st = "Knap A"; }
+				if (event.getSource() == btnB) { st = "Knap B"; }
+				if (event.getSource() == btnC) { st = "Knap C"; }
+				if (event.getSource() == btnD) { st = "Knap D"; }
+				if (event.getSource() == btnStar) { st = "Knap Star"; }
+				if (event.getSource() == btnHash) { st = "Knap Hash"; }
+				statusLabel.setText("Status: " + st);
+			}
+		}
+		
+		btn0.addActionListener(new ButtonListener());
+		btn1.addActionListener(new ButtonListener());
+		btn2.addActionListener(new ButtonListener());
+		btn3.addActionListener(new ButtonListener());
+		btn4.addActionListener(new ButtonListener());
+		btn5.addActionListener(new ButtonListener());
+		btn6.addActionListener(new ButtonListener());
+		btn7.addActionListener(new ButtonListener());
+		btn8.addActionListener(new ButtonListener());
+		btn9.addActionListener(new ButtonListener());
+		btnA.addActionListener(new ButtonListener());
+		btnB.addActionListener(new ButtonListener());
+		btnC.addActionListener(new ButtonListener());
+		btnD.addActionListener(new ButtonListener());
+		btnHash.addActionListener(new ButtonListener());
+		btnStar.addActionListener(new ButtonListener());
+	}
+	
+	public JPanel makePanel() {
+		btn0.setLabel("0");
+		btn1.setLabel("1");
+		btn2.setLabel("2");
+		btn3.setLabel("3");
+		btn4.setLabel("4");
+		btn5.setLabel("5");
+		btn6.setLabel("6");
+		btn7.setLabel("7");
+		btn8.setLabel("8");
+		btn9.setLabel("9");
+		btnA.setLabel("A");
+		btnB.setLabel("B");
+		btnC.setLabel("C");
+		btnD.setLabel("D");
+		btnStar.setLabel("*");
+		btnHash.setLabel("#");
+		
+		statusLabel.setText("Status: ");
+		
+		dPanel = new JPanel();
+		dPanel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 0;
+		c.gridy = 0;
+		dPanel.add(btn7, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 1;
+		c.gridy = 0;
+		dPanel.add(btn8, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 2;
+		c.gridy = 0;
+		dPanel.add(btn9, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 3;
+		c.gridy = 0;
+		dPanel.add(btnA, c);
+		
+		
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 0;
+		c.gridy = 1;
+		dPanel.add(btn4, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 1;
+		c.gridy = 1;
+		dPanel.add(btn5, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 2;
+		c.gridy = 1;
+		dPanel.add(btn6, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 3;
+		c.gridy = 1;
+		dPanel.add(btnB, c);
+		
+		
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 0;
+		c.gridy = 2;
+		dPanel.add(btn1, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 1;
+		c.gridy = 2;
+		dPanel.add(btn2, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 2;
+		c.gridy = 2;
+		dPanel.add(btn3, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 3;
+		c.gridy = 2;
+		dPanel.add(btnC, c);
+		
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 0;
+		c.gridy = 3;
+		dPanel.add(btnStar, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 1;
+		c.gridy = 3;
+		dPanel.add(btn0, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 2;
+		c.gridy = 3;
+		dPanel.add(btnHash, c);
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 3;
+		c.gridy = 3;
+		dPanel.add(btnD, c);
+
+
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 0;
+		c.gridy = 4;
+		c.gridwidth = 3;
+		c.ipady = 4;
+		dPanel.add(statusLabel, c);
+		
+		return dPanel;
+	}
+	
+	public JFrame makeFrame() {
+		JFrame frame = new JFrame(windowTitle);
+		
+		frame.setSize(175, 175);
+		frame.setLocation(450,250);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		return frame;
+	}
+}
